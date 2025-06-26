497b9dec07398064bf9eeba2ddb9cb9b4f04aa5c
liamcahill <cahill.liam220@gmail.com>
2025-06-26 14:11:27 -0400

    add Postcard class; begin building.

--- reddit-clone/src/components/PostCard.tsx
+++ reddit-clone/src/components/PostCard.tsx
@@ -6,6 +6,9 @@ import {api} from "../../convex_generated/api"
 import {Id} from "../../convex_generated/dataModel"
 import {useUser} from "@clerk/clerk-react"
 import "../styles/PostCard.css"
+import { create } from "domain"
+import { userPosts } from "../../convex/post"
+import { useState } from "react"
 
 interface Post {
     _id: Id<"post">,
@@ -42,6 +45,76 @@ interface PostContentProps {
 }
 
 const PostHeader = ({author, subreddit, showSubreddit, creationTime}: PostHeaderProps) => {
-    
-     
+    return (
+    <div className="post-header">
+        {author ? (
+            <Link to={`/u/${author.username}`}>u{author?.username}</Link>
+         ) : (
+         <span className="post-author">u/deleted</span>
+        )}
+        {showSubreddit && subreddit && (
+            <> 
+            <span className="post-dot">-</span>
+            <Link to={`/r/${subreddit.name}`} className="post-subreddit">
+                r/{subreddit.name}
+            </Link> 
+        </>
+        )}
+        <span className="post-dot">-</span>
+        <span className="post-timestamp">
+            {new Date(creationTime).toLocaleString()}
+        </span>
+    </div>
+    );
+}
+
+const PostContent = ({subject, body, image, expandedView}: PostContentProps) => {
+    return (
+    <>
+        {expandedView ? (
+            <>
+            <h1 className="post-title">{subject}</h1>
+            {image && (
+                <div className="post-image-container">
+                    <img src={image} alt="Post content" className="post-image"/>
+                </div>
+                )}
+                {body && <p className="post-body">{body}</p>}
+        </> 
+        ) : (
+            <div className="preview-post">
+                <div>
+                    <h2 className="post-title">{subject}</h2>
+                    {body && <p className="post-body">{body}</p>}
+                </div>
+                {image &&(
+                    <div className="post-image-container small-img">
+                    <img src={image} alt="Post content" className="post-image"/>
+                </div>
+                )}
+        </div>)}
+    </>
+    );
+};
+
+const PostCard = ({post, showSubreddit, expandedView}: PostCardProps) => {
+    const [showComments, setShowComments] = useState(expandedView)
+    const navigate = useNavigate()
+    const {user} = useUser();
+    const ownedByCurrentUser = post.author?.username === user?.username;
+
+    const handleComment = () => {}
+
+    const handleDelete = () => {
+
+    }
+
+    const handleSubmit = (content: string) => {}
+
+    return <div className={`post-card ${expandedView ? "expanded" : ""}`}>
+        <div className="post=content">
+            <PostHeader />
+            <PostContent />
+        </div>
+    </div>
 }
